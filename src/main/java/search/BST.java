34b9fc20afbe528a404a86f4e1ae1ddfd7891f4f
Bloss <1248212664@qq.com>
2016-08-26 01:46:10 +0800

    update

--- src/main/java/search/BST.java
+++ src/main/java/search/BST.java
@@ -67,4 +67,70 @@ public class BST<Key extends Comparable<Key>, Value> {
         x.size = 1 + size(x.left) + size(x.right);
         return x;
     }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if(x.left == null)
+            return x;
+        return min(x.left);
+    }
+
+    public Key max() {
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if(x.right == null)
+            return x;
+        return max(x.right);
+    }
+
+    public Key floor(Key key) {
+        if(key == null)
+            throw new NullPointerException("argument to floor() is null");
+        Node x = floor(root, key);
+        if(x == null)
+            return null;
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if(x == null)
+            return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp == 0)
+            return x;
+        if(cmp < 0)
+            return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if(t == null)
+            return x;
+        return t;
+    }
+
+    public Key ceil(Key key) {
+        if(key == null)
+            throw new NullPointerException("argument to floor() is null");
+        Node x = ceil(root, key);
+        if(x == null)
+            return null;
+        return x.key;
+    }
+
+    private Node ceil(Node x, Key key) {
+        if( x == null)
+            return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp == 0)
+            return x;
+        if(cmp > 0)
+            return ceil(x.right, key);
+        Node t = ceil(x.left, key);
+        if(t == null)
+            return x;
+        return t;
+    }
 }
